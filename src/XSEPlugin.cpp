01d5b2a976fbd3e9f780c2731177246e0754782b
Tim <15017472+doodlum@users.noreply.github.com>
2022-09-02 15:37:58 +0100

    Fix UI not showing

--- src/XSEPlugin.cpp
+++ src/XSEPlugin.cpp
@@ -15,6 +15,7 @@ static void MessageHandler(SKSE::MessagingInterface::Message* a_msg)
 				switch (calltype) {
 				case ENBCallbackType::ENBCallback_PostLoad:
 					Reflex::GetSingleton()->LoadJSON();
+					Reflex::GetSingleton()->RefreshUI();
 					break;
 				case ENBCallbackType::ENBCallback_PreSave:
 					Reflex::GetSingleton()->SaveJSON();
@@ -61,7 +62,7 @@ void InitializeLog()
 #ifndef NDEBUG
 	const auto level = spdlog::level::trace;
 #else
-	const auto level = spdlog::level::debug;
+	const auto level = spdlog::level::info;
 #endif
 
 	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
